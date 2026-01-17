1f5badc113a270b750f728b2a2ccded2cbc77831
Ratan Kumar <krratan80@gmail.com>
2026-01-17 22:13:28 +0530

    add plan

--- app/(main)/admin/plans/create/page.tsx
+++ app/(main)/admin/plans/create/page.tsx
@@ -1,5 +1,8 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import Input2Comp from "@/components/ui/Input2Comp";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 import React, { useState } from "react";
 interface FormData {
   name: string;
@@ -20,19 +23,29 @@ export default function CreatePlan() {
     validity: 0,
     active: true,
   });
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("data: ", formData);
+  }
   return (
     <div className="w-full min-h-screen p-4 mx-auto space-y-6">
       <div className="max-w-2xl mx-auto bg-card p-4 rounded-md space-y-4">
-        <div className="">
-          Plan
+        <h1 className="text-base font-bold tracking-wider">Plan</h1>
+        <div className="flex items-center justify-between space-x-2">
+          <Label htmlFor="airplane-mode">Active </Label>
+          <Switch id="active" checked={formData.active} onCheckedChange={() => setFormData((prev) => ({ ...prev, active: !formData.active }))} />
         </div>
+        <form className="space-y-4" onSubmit={handleFormSubmit}>
         <Input2Comp
           disabled
           lable="Name"
           lablefor="name"
           type="text"
           value={formData.name}
-          onChange={(e) => setFormData((prev) => ({...prev, name: e.target.value }))}
+          onChange={(e) =>
+            setFormData((prev) => ({ ...prev, name: e.target.value }))
+          }
           key={"name"}
         />
         <Input2Comp
@@ -40,10 +53,59 @@ export default function CreatePlan() {
           lable="Price"
           lablefor="price"
           type="text"
+          preValue="EURO"
           value={formData.price}
-          onChange={(e) => setFormData((prev) => ({...prev, price: Number(e.target.value) }))}
-          key={"name"}
+          onChange={(e) =>
+            setFormData((prev) => ({ ...prev, price: Number(e.target.value) }))
+          }
+          key={"price"}
         />
+        <Input2Comp
+          disabled
+          lable="ROI"
+          lablefor="roi"
+          type="text"
+          value={formData.roi}
+          subValue={"%"}
+          onChange={(e) =>
+            setFormData((prev) => ({ ...prev, roi: Number(e.target.value) }))
+          }
+          key={"roi"}
+        />
+        <Input2Comp
+          disabled
+          lable="Maximum Return"
+          lablefor="maximumReturn"
+          type="text"
+          value={formData.maximumReturn}
+          subValue={"%"}
+          onChange={(e) =>
+            setFormData((prev) => ({
+              ...prev,
+              maximumReturn: Number(e.target.value),
+            }))
+          }
+          key={"maximumReturn"}
+        />
+        <Input2Comp
+          disabled
+          lable="Validity"
+          lablefor="validity"
+          type="text"
+          value={formData.validity}
+          subValue={"Days"}
+          onChange={(e) =>
+            setFormData((prev) => ({
+              ...prev,
+              validity: Number(e.target.value),
+            }))
+          }
+          key={"validity"}
+        />
+        <Button type="submit" className="bg-sky-500 hover:bg-sky-600 w-full">
+          Submit
+        </Button>
+        </form>
       </div>
     </div>
   );
--- components/ui/Input2Comp.tsx
+++ components/ui/Input2Comp.tsx
@@ -5,8 +5,9 @@ interface Input2Compprop {
   lable: string;
   lablefor: string;
   type: "text" | "email";
+  preValue?: string;
   value: string | number;
-  subValue?: string | number;
+  subValue?: string;
   onChange: ChangeEventHandler<HTMLInputElement>;
   disabled?: boolean;
 }
@@ -16,6 +17,7 @@ export default function Input2Comp({
   lablefor,
   type,
   disabled,
+  preValue,
   value,
   subValue,
   onChange,
@@ -23,6 +25,7 @@ export default function Input2Comp({
   return (
     <>
       <div className="relative">
+        {/* input */}
         <input
           id={lablefor}
           type={type}
@@ -31,27 +34,44 @@ export default function Input2Comp({
           onChange={onChange}
           placeholder=" "
           className={cn(
-            "peer h-12 w-full rounded-md border px-3 pt-4 text-base outline-none transition-all",
+            "peer h-12 w-full rounded-md border px-3 pt-3.5 text-base outline-none transition-all",
             "disabled:cursor-not-allowed disabled:text-muted-foreground",
             "focus:border-blue-600 focus:ring-1 focus:ring-blue-600/20",
-            "placeholder:text-transparent"
+            "placeholder:text-transparent",
+            preValue && "pl-16",
           )}
         />
+
+        {/* Main input label */}
         <label
           htmlFor={lablefor}
           className={cn(
             "absolute left-3 top-1 z-10 text-xs text-muted-foreground transition-all duration-200",
             "peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-muted-foreground/70",
-            "peer-focus:top-1 peer-focus:text-xs"
+            "peer-focus:top-1 peer-focus:text-xs",
           )}
         >
           {lable}
         </label>
+
+        {/* first/pre input lable */}
+        {preValue && (
+          <span
+            className={cn(
+              "absolute text-base tracking-wide left-3 top-3 pt-2 text-muted-foreground",
+              !disabled && "text-muted-foreground/70",
+            )}
+          >
+            {preValue}
+          </span>
+        )}
+
+        {/* last input labal */}
         {subValue && (
           <span
             className={cn(
               "absolute text-base tracking-wide right-3 top-3 text-muted-foreground",
-              !disabled && "text-muted-foreground/70"
+              !disabled && "text-muted-foreground/70",
             )}
           >
             {subValue}
